7e01a38acbac7ed6d4d2513e99a837b0d9bf2447
isaactrico <47197446+isaactrico@users.noreply.github.com>
2019-02-21 11:39:57 -0600

    Add files via upload

--- pointlessPointerClassyCalculator.cpp
+++ pointlessPointerClassyCalculator.cpp
@@ -1,4 +1,4 @@
-
+#include <string>
 #include <iostream>
 #include <cstdlib>
 
@@ -33,14 +33,19 @@ class Calculadora{
     int ans;
     int A[50];
     char *op;
+    string str;
   public:
     void setN1(int *n1);
     void setN2(int *n2);
     void setOp(char *op);
     void convert();
     int* operate();
+    void setStr(string *str);
 };
 
+void Calculadora::setStr(string *str){
+  this->str=*str;
+}
 
 void Calculadora::setN1(int *n1){
   this->n1=n1;
@@ -118,8 +123,51 @@ int* Calculadora::operate(){
     n3=&ans;
     return n3;
   }
+  //hexa a decimal
+  this->ans=0;
+  int aum=1;
+  if(*this->op=='H'){
+    for(int i=(this->str.length())-1; i>=0; i--){
+      if(str[i]=='1')
+        ans+= 1*aum;
+      if(str[i]=='2')
+        ans+= 2*aum;
+      if(str[i]=='3')
+        ans+= 3*aum;
+      if(str[i]=='4')
+        ans+= 4*aum;
+      if(str[i]=='5')
+        ans+= 5*aum;
+      if(str[i]=='6')
+        ans+= 6*aum;
+      if(str[i]=='7')
+        ans+= 7*aum;
+      if(str[i]=='8')
+        ans+= 8*aum;
+      if(str[i]=='9')
+        ans+= 9*aum;
+      if(str[i]=='A')
+        ans+= 10*aum;
+      if(str[i]=='B')
+        ans+= 11*aum;
+      if(str[i]=='C')
+        ans+= 12*aum;
+      if(str[i]=='D')
+        ans+= 13*aum;
+      if(str[i]=='E')
+        ans+= 14*aum;
+      if(str[i]=='F')
+        ans+= 15*aum;
+      aum=aum*16;
+    }
+    n3=&ans;
+    return n3;
+  }
+
+
 }
 void Calculadora::convert(){
+
   //decimal a binario
   if(*this->op=='d'){
     int i = 0;
@@ -162,29 +210,47 @@ int main(){
   int *p1, *p2, *p3;
   char ope;
   char *op;
+  string hex;
+  string *h;
 
   do {
-    std::cout << "Ingrese el primer numero: " <<endl;
-    cin>>num1;
-    p1=&num1;
-    cal.setN1(p1);
-
-    cout<<"Ingrese el segundo numero: "<<endl;
-    cin>>num2;
-    p2=&num2;
-    cal.setN2(p2);
-
     cout<<"Elija uno de los siguientes operadores:\n| + suma     |   - resta    |    * multiplicacion   |    / division   |\n| ^ potencia | ! factorial (solo toma en cuenta el primer numero ingresado) |\n| d decimal a binario | b binario a decimal |\n| o decimal a octal | O octal a decimal |\n| h decimal a hexadecimal | H hexadecimal a decimal |\n"<<endl;
     cin>>ope;
     op=&ope;
     cal.setOp(op);
 
     if(ope == '+' || ope == '-' || ope == '/' || ope == '*' || ope == '^' || ope == '!' || ope == 'b' || ope == 'O'){
+      std::cout << "Ingrese el primer numero: " <<endl;
+      cin>>num1;
+      p1=&num1;
+      cal.setN1(p1);
+
+      cout<<"Ingrese el segundo numero: "<<endl;
+      cin>>num2;
+      p2=&num2;
+      cal.setN2(p2);
+
+      std::cout << "Resultado: " <<*cal.operate()<<endl;
+    }else if(ope == 'H'){
+      cout<<"Ingrese su valor hexadecimal a convertir: "<<endl;
+      cin>>hex;
+      h = &hex;
+      cal.setStr(h);
+
       std::cout << "Resultado: " <<*cal.operate()<<endl;
     }else{
+      std::cout << "Ingrese el primer numero: " <<endl;
+      cin>>num1;
+      p1=&num1;
+      cal.setN1(p1);
+
+      cout<<"Ingrese el segundo numero: "<<endl;
+      cin>>num2;
+      p2=&num2;
+      cal.setN2(p2);
+
       cal.convert();
     }
-
     std::cout << "Ingrese \n0 para terminar \n1 para realizar una operacion " <<endl;
     cin>>rep;
   } while(rep!=0);
